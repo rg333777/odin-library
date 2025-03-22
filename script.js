d39f43e3fd417ab325349cd83f47920d5b56136f
rg333777 <143126002+rg333777@users.noreply.github.com>
2025-03-22 13:36:17 +0000

    added constructor for book object

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('test')
+const myLibrary = []
+
+
+function Book(name, author, pages, status) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.info = () => {
+        return this.name + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.status
+    }
+
+}
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'unread')
+console.log(theHobbit.info())
+
+
+function addBookToLibrary() {
+
+}
